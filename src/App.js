93964d2112bb421ae15405debdbd3cac5d8835c1
Joseph Oravbiere <josepho.dev@gmail.com>
2020-11-22 00:18:16 -0500

    Add board and status columns

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import KanbanDB from 'kanbandb';
+import React from "react"
+import "./App.css"
+import KanbanDB from "kanbandb"
 
-function App () {
+const Status = ({ name }) => (
+  <div className="status">
+    <p className="status-name">{name}</p>
+  </div>
+)
+
+const Board = ({ children }) => <div className="board">{children}</div>
+
+function App() {
   return (
-    <div></div>
+    <div className="container">
+      <Board>
+        <Status name="To-do" />
+        <Status name="In Progress" />
+        <Status name="Done" />
+      </Board>
+    </div>
   )
 }
 
-export default App;
+export default App
 
 // async function initialize() {
 //   /**
 //    * Use KanbanDB like so (but you might want to move it) - types are provided
 //    * by jsconfig.json, which will utilize d.ts files and give you autocompletion for
 //    * KanbanDB, in Visual Studio Code, if that is your preferred IDE.
-//    * 
+//    *
 //    * This code (initialize function) is for demonstration only.
 //    */
 //   const instance = await KanbanDB.connect(null);
